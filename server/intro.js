be2894a8195f6dc75963434d43f5bee7efb46c50
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-02-07 14:48:12 -0600

    everything works.

--- server/intro.js
+++ server/intro.js
@@ -11,13 +11,13 @@ const port = 4141;
 
 // app.listen(port, fPeopleDisplay);
 // function fPeopleDisplay(){
-//     let peopleModule = require('/Users/ryanudelhoven/Documents/T2/gacrux-node-intro/server/public/scripts/people-module.js');
+//     let peopleModule = require('./people-module.js');
 //     console.log(peopleModule);
 //     peopleModule.displayPeople();
 // } // end fPeopleDisplay
 
 app.listen(port, fColorDisplay);
 function fColorDisplay(){
-    let colors = require('/Users/ryanudelhoven/Documents/T2/gacrux-node-intro/server/public/scripts/testFile.js');
+    let colors = require('./testFile.js');
     console.log(colors);
 } // end fColorDisplay
